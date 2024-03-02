2db15cc0e48a8ebe068f6ec3fce4cd27b2d7ee30
Amandeep Kochhar <amandeeptherockstar@gmail.com>
2024-03-02 17:31:43 +0530

    fixed errors

--- ui/lib/components/Button/index.tsx
+++ ui/lib/components/Button/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styles from "./styles.module.css";
 
 export function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
--- ui/lib/components/Label/index.tsx
+++ ui/lib/components/Label/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styles from "./styles.module.css";
 
 export function Label(props: React.LabelHTMLAttributes<HTMLLabelElement>) {
